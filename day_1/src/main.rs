7879c0069ebcb75791ff88d64a4fb641c04d9377
Shaojieee <shaojieee@gmail.com>
2024-12-03 23:08:56 +0800

    refactor

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,19 +1,25 @@
+use std::time::Instant;
 use std::collections::BTreeMap;
-use std::path::Path;
-use std::fs;
 
 fn main() {
-    q1_a(Path::new("./y2024/input/day1.txt"));
-    q1_b(Path::new("./y2024/input/day1.txt"));
+    
+    let now = Instant::now();
+    let r1 = p1();
+    let elapsed = now.elapsed();
+    println!("Result for p1: {}", r1);
+    println!("part1 : {:.2?}", elapsed);
+
+    let now = Instant::now();
+    let r2 = p2();
+    let elapsed = now.elapsed();
+    println!("Result for p2: {}", r2);
+    println!("part1 : {:.2?}", elapsed);
 }
 
-fn read_input(input: &Path) -> String {
-    fs::read_to_string(input).expect("Unable to read file")
-}
 
 
-fn q1_a(input: &Path) {
-    let input_str = read_input(input);
+fn p1() -> i64 {
+    let input_str = include_str!("./input.txt");
     
     let mut rows: [Vec<i64>; 2] = [vec![], vec![]];
     for (i, num) in input_str.split_whitespace().enumerate() {
@@ -22,21 +28,19 @@ fn q1_a(input: &Path) {
 
     rows.iter_mut().for_each(|row| row.sort());
 
-    let result: i64 = rows[0]
+    rows[0]
         .iter()
         .zip(&rows[1])
         .map(|(x1, x2)| (x1 - x2).abs())
-        .sum();
-
-    println!("Result for q1_a: {}", result);
+        .sum()
 }
 
 
-fn q1_b(input: &Path) {
+fn p2() -> i64 {
     let mut left: BTreeMap<i64, i64> = BTreeMap::new();
     let mut right: BTreeMap<i64, i64> = BTreeMap::new();
 
-    read_input(input)
+    include_str!("./input.txt")
         .split_whitespace()
         .enumerate()
         .for_each(|(i, x)| {
@@ -45,7 +49,5 @@ fn q1_b(input: &Path) {
             *map.entry(x).or_insert(0) += 1;
         });
 
-    let results: i64 = left.iter().map(|(x,y)| x * y * right.get(x).unwrap_or(&0)).sum();
-
-    println!("Result for q1_b: {}", results)
+    left.iter().map(|(x,y)| x * y * right.get(x).unwrap_or(&0)).sum()
 }
